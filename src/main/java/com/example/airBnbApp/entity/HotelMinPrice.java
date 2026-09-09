831ddfeed2cb86f733317d4c3c417a766df8ef22
Sumer <sumerinusmaner1711@gmail.com>
2026-09-09 23:30:24 +0530

    Pricing strategy updated

---
+++ src/main/java/com/example/airBnbApp/entity/HotelMinPrice.java
@@ -0,0 +1,45 @@
+package com.example.airBnbApp.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class HotelMinPrice {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private LocalDate date;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "hotel_id",nullable = false)
+    private Hotel hotel;
+
+    @Column(nullable = false,precision = 10,scale = 2)
+    private BigDecimal price; //cheapest room price on a particular day
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
+
+    @CreationTimestamp
+    private LocalDateTime updatedAt;
+
+    public HotelMinPrice(Hotel hotel, LocalDate date) {
+        this.hotel=hotel;
+        this.date=date;
+    }
+}
